264c0d2784a8c7fc7f745cb543ef4198b9302e4b
Chris Fritsch <fritsch9999@gmail.com>
2021-06-14 11:46:01 +0200

    Relocated the js stuff.

--- sites/views/routes.php
+++ sites/views/routes.php
@@ -1,5 +1,5 @@
 <?php include("template.php");?>
-
+<?php include("javascript.js");?>
 
 
 <div class="container">
@@ -32,53 +32,3 @@
     </div>
 </div>
 
-<script>
-    var routes = <?php echo json_encode($routes); ?>;
-    var destinations = <?php echo json_encode($destinations); ?>;
-
-    function showRoute(key) {
-        cardContainer = document.getElementById('test');
-        
-        
-        while (cardContainer.hasChildNodes()) {
-            cardContainer.removeChild(cardContainer.lastChild);
-        }
-
-        routeTitle = document.createElement('h5');
-        routeTitle.innerText = "Route " + key;
-        cardContainer.appendChild(routeTitle);
-
-        routes[key].forEach((destination) => {
-            createRouteCard(destination);
-        });
-    }
-
-    function getRouteText(id){
-        destination = destinations[id-1];
-        if(destination.diff > 0){
-            return "Abholen: " + destination.diff;
-        }
-        return "Abstellen: " + Math.abs(destination.diff);
-    }
-
-    let createRouteCard = (destination) => {
-        let card = document.createElement('div');
-        card.className = 'card shadow cursor-pointer';
-
-        let cardBody = document.createElement('div');
-        cardBody.className = 'card-body';
-
-        let title = document.createElement('h5');
-        title.innerText = destination.name;
-        title.className = 'card-title';
-
-        let text = document.createElement('p');
-        text.innerText = getRouteText(destination.id);
-        text.className = 'card-text';
-
-        cardBody.appendChild(title);
-        cardBody.appendChild(text);
-        card.appendChild(cardBody);
-        cardContainer.appendChild(card);
-    }
-</script>
